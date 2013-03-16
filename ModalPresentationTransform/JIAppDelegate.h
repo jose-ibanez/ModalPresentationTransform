e9ff24c8d3e1e940d644c8d15d2c25d2e2e91c6e
Jose Ibanez <jose.ibanez@gmail.com>
2013-03-16 08:28:11 -0400

    Initial Commit

---
+++ ModalPresentationTransform/JIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JIAppDelegate.h
+//  ModalPresentationTransform
+//
+//  Created by Jose Ibanez on 3/16/13.
+//  Copyright (c) 2013 Jose Ibanez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ModalPresentationTransform/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ModalPresentationTransform
+//
+//  Created by Jose Ibanez on 3/16/13.
+//  Copyright (c) 2013 Jose Ibanez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JIAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JIAppDelegate class]));
+    }
+}
